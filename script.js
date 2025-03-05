d599f97230b9e42130d425d1212578b96537fa67
Kiran Tikoo <kirantikoo@gmail.com>
2025-03-05 22:11:17 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("loginForm").addEventListener("submit", function (event) {
+        var email = document.getElementById("email").value.trim();
+        var password = document.getElementById("password").value.trim();
+        var emailError = document.getElementById("emailError");
+        var passwordError = document.getElementById("passwordError");
+
+        // Clear previous errors
+        emailError.innerText = "";
+        passwordError.innerText = "";
+
+        let valid = true; // Flag to check form validity
+
+        // Check if email is empty
+        if (email === "") {
+            emailError.innerText = "Email is required.";
+            valid = false;
+        }
+
+        // Check if password is empty
+        if (password === "") {
+            passwordError.innerText = "Password is required.";
+            valid = false;
+        }
+
+        // Prevent form submission if validation fails
+             if (!valid) {
+            event.preventDefault();
+        } 
+            else {
+            event.preventDefault();
+            window.location.href = "userdashboard.html";
+        }
+    });
+});
